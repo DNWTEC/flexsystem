6329fd96573cb478e03cf393b83a14bdde1515d3
ronecho <rone@dnwinter.com>
2016-08-10 21:02:13 +0800

    Add one file: SayHello.

--- src/main/java/com/chochae/afes/HomeController.java
+++ src/main/java/com/chochae/afes/HomeController.java
@@ -35,5 +35,18 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@RequestMapping(value = "/sayhello", method = RequestMethod.GET)
+	public String sayHello(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("sayHello", "Hello, Stupid!" );
+		
+		return "sayHello";
+	}	
 }
